37c8dccf386f2194f9e2c1dd8c1ec7313c01dd97
Daniele <Daniele@Psych-II.local>
2015-02-07 01:46:17 +0100

    validation email

---
+++ Example/G8MaterialKitTextField/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  G8MaterialKitTextField
+//
+//  Created by Daniele on 07/02/15.
+//  Copyright (c) 2015 Daniele Galiotto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textField1: G8MKTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
